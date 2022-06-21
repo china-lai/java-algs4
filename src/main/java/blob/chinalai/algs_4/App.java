e13e5b12e12b7fa1cbd2ead322edd427118b4b5f
lai_wen_jie <1626282810@qq.com>
2022-06-21 21:46:58 +0800

    1. 文件

--- src/main/java/blob/chinalai/algs_4/App.java
+++
@@ -1,7 +0,0 @@
-package blob.chinalai.algs_4;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("算法第四版");
-    }
-}
--- src/main/java/blob/chinalai/core_one/App.java
+++
@@ -1,8 +0,0 @@
-package blob.chinalai.core_one;
-
-public class App {
-
-    public static void main(String[] args) {
-        System.out.println("Java核心技术卷一");
-    }
-}
